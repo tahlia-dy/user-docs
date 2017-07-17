fb704243bae2a086ec3f70e3ec78a65f6b002b1f
Tahlia Dysart <tdysart@yozu.co.uk>
2017-07-17 14:18:40 +0100

    Made the code simpler and redid the nav, populated pages with a template

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,16 @@
 class StaticPagesController < ApplicationController
   def index
   end
- 
+  
+  def players
+  end
+
+  def items
+  end
+
+  def scenes
+  end 
+
+  def how_to
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
+  get 'scenes', to: 'static_pages#scenes'
+  get '/items', to: 'static_pages#items'
+  get '/players', to: 'static_pages#players'
+  get '/how_to', to: 'static_pages#how_to'
 end
 
 
